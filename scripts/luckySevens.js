0058513eef175a1b85971db0e22beba93bc45bbf
Seth Harbin <47837530+SethHarbin@users.noreply.github.com>
2019-03-01 23:07:07 -0600

    Add files via upload

---
+++ scripts/luckySevens.js
@@ -0,0 +1,49 @@
+function validateBet() {
+	clearErrors();
+	var initialBet = document.forms["game"]["bet"].value;
+	var numBet = parseFloat(document.forms["game"]["bet"].value.substr(1));
+	var highBet = numBet;
+	var count = 0;
+	var highCount = 0;
+	if(numBet == 0){
+		alert("Bet should not be $0.00.");
+		documents.forms["game"]["bet"].parentElement.className = "form-group has-error";
+		documents.forms["game"]["bet"].focus();
+		return false;
+	}
+	do{
+		count++;
+		if(((Math.floor(Math.random() * 6) + 1)+(Math.floor(Math.random() * 6) + 1))==7){
+			numBet = numBet+4.00;
+			if(numBet > highBet){
+				highCount = count;
+				highBet = numBet;
+			}
+		}else{
+			numBet--;
+		}
+	}while(numBet>0)
+	
+	document.getElementById("results").style.display = "block";
+	document.getElementById("initial").innerText = initialBet;
+	document.getElementById("rollCount").innerText = count;
+	document.getElementById("maxBet").innerText = "$"+ parseFloat(highBet).toFixed(2);
+	document.getElementById("maxBetRolls").innerText = highCount;
+	
+	return false;
+	
+}
+
+function resetGame() {
+	clearErrors();
+	document.forms["game"]["bet"].value = "$0.00";
+	document.getElementById("results").style.display = "none";
+	document.getElementById("submitButton").innerText = "Play";
+	document.forms["game"]["bet"].focus();
+}
+
+function clearErrors() {
+    if (document.forms["game"]["bet"].parentElement.className.indexOf("has-") != -1) {
+        document.forms["game"].elements[i].parentElement.className = "form-group";
+    }
+} 
